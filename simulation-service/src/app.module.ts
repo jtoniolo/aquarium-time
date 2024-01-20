14d33e00f27e8b3f08e9075144543bbd5cee5f10
Jeffrey Toniolo <6921365+jtoniolo@users.noreply.github.com>
2024-01-20 16:01:38 -0500

    Update Docker image tags and add schedule and config modules Add logger to NestFactory.create() in main.ts Export MqttService in MqttModule

--- simulation-service/src/app.module.ts
+++ simulation-service/src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppService } from './app.service';
 import { SunModule } from './sun/sun.module';
-import { MqttModule } from './mqtt/mqtt.module';
+import { ScheduleModule } from '@nestjs/schedule';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [MqttModule, SunModule],
+  imports: [ScheduleModule.forRoot(), SunModule, ConfigModule.forRoot()],
 
   providers: [AppService],
 })
