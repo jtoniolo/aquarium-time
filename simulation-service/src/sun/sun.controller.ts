9c71ec57bf03edf82ba4d9832fcee807b2c8ace2
Jeffrey Toniolo <6921365+jtoniolo@users.noreply.github.com>
2024-01-21 16:02:52 -0500

    Add Dockerignore file and update app module

--- simulation-service/src/sun/sun.controller.ts
+++ simulation-service/src/sun/sun.controller.ts
@@ -1,12 +1,14 @@
 import { Controller, Get } from '@nestjs/common';
-import { SunService } from './sun.service';
+import { DistribuitonData, SunService } from './sun.service';
 
 @Controller('suns')
 export class SunController {
   constructor(private readonly sunService: SunService) {}
 
   @Get()
-  async getDistribution() {
-    return this.sunService.getDistributionData();
+  async getDistribution(): Promise<DistribuitonData[]> {
+    const data = this.sunService.getDistributionData();
+    console.log(data);
+    return data;
   }
 }
--- simulation-service/src/sun/sun.model.ts
+++ simulation-service/src/sun/sun.model.ts
@@ -1,5 +1,16 @@
 // This class represents a simulated sun with properties for brightness and RGBW color values at a specific moment in time.
 export class SimulatedSun {
+  constructor() {
+    this.brightness = 0;
+    this.rgbw = {
+      red: 0,
+      green: 0,
+      blue: 0,
+      white: 0,
+    };
+  }
+
+  on: boolean; // Whether the simulated sun is on or off
   brightness: number; // The brightness of the simulated sun, a value between 0 and 100
   rgbw: RGBW; // The RGBW color values of the simulated sun
 }
@@ -16,6 +27,7 @@ export class RGBW {
 export class SunConfig {
   sunRiseTime?: Time; // The time of sunrise
   sunDuration?: Time; // The duration of the sun
+  highLightRatio?: number = 1 / 3; // The ratio which determines the lenght of midday, the brightest part of the day. Should be 0 < highLightRatio <= 1
   //location?: Location; // The location for the sun simulation
   sunriseOffset?: number; // The offset for the sunrise time
   durationMultiplier?: number; // The multiplier for the duration of the sun
